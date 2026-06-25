3ce12dc299dca289beefe1472959286845e33e8e
Ashutosh <ashu.deshmukh560@gmail.com>
2026-06-25 07:22:42 +0530

    Initial Commit

---
+++ src/Enrollment/EmployeeSalary.java
@@ -0,0 +1,4 @@
+package Enrollment;
+
+public class EmployeeSalary {
+}
---
+++ src/Options/ChooseOption.java
@@ -0,0 +1,9 @@
+package Options;
+
+public class ChooseOption {
+    public void showOptionsToUser(){
+        System.out.println("Choose Among below Options:");
+        System.out.println("1. Enroll Employee");
+        System.out.println("2. Leave Calender");
+    }
+}
